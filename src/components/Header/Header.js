664af7a179a8594525bb96fe054b6051c6584d63
Vinay Bandi <Vinay.bandi@cardinal-ip.com>
2021-11-06 19:04:28 -0500

    added tab style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -58,7 +58,7 @@ function Header(props) {
                 textColor="primary"   
                 variant="scrollable"
                 scrollButtons="auto"
-                
+                indicatorColor="primary"
             >
                 <Tab label="Home" />
                 <Tab label="About" />
